0b13c85f26dd2169e6425267b5fa16c3ef17b92d
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-06-25 11:57:27 -0500

    Se agrego la lista de tajetas con una sencilla con sus alineaciones interesantes

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -10,6 +10,58 @@ class CardPage extends StatelessWidget {
         title: Text('Salu2Internautas'),
       ),
 
+      body: ListView(
+
+        padding: EdgeInsets.all(15.0) , //Separa el contenido del List en todos sus bordes con sus limites lateres 20
+
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }//build CardPage
+
+  Widget _cardTipo1(){
+    return Card(//Widget card de la lib material
+      child: Column(
+        children: <Widget>[
+
+          ListTile(
+            leading : Icon(Icons.all_inclusive , color: Colors.blue[800]), //Azul oscuro xd
+            title: Text('Rex es un perro beibe'),
+            subtitle: Text("Todo esto es para ver que se le puede agregar mucho texto sin problemas en el subtitle "),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end, //Alinia todo al final del row
+
+            children: <Widget>[
+
+              FlatButton(
+                onPressed: (){
+
+                },
+
+                child: Text("Cancelar"),
+              ),
+
+              FlatButton(
+                onPressed: (){
+
+                },
+
+                child: Text("Aceptar"),
+              ),
+
+            ],
+          )
+
+        ],
+        ),
     );
   }
-}
+
+
+
+}//class CardPage
+
