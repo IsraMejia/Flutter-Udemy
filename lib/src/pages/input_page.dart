9eb6f9a018ae56fca7f5e89ec5abffac4dff6bf7
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-06-26 20:15:44 -0500

    Se agrego el email y password

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -11,6 +11,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
 
   String _direccion ='' ;
+  String _email ='';
+  String _password='';
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +28,13 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           _crearInput(),
           Divider(), //Ocupa el espacio que sobra
+          _crearEmail(),
+          Divider(),
+          _ingresaPassword(),
+          Divider(),
+
           _revisarDatos(),
+          
           
         ],
       ),
@@ -64,13 +72,61 @@ class _InputPageState extends State<InputPage> {
     );
   } //_crear input
 
+  Widget _crearEmail(){
+    return TextField(
 
+      keyboardType: TextInputType.emailAddress ,
+      //Te pone el arroba en el teclado xD
+      
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.only( bottomLeft : Radius.circular(30)) ,
+        ) ,
+        counter: Text('letras ${_direccion.length } '),
+        labelText: 'email@gmail.com',
+        suffixIcon: Icon(Icons.email , size: 30.0 )
+      ),
+
+      onChanged: (textoEscrito){ //recibe un valor el cual se definio en esta linea
+        setState( ()  {
+          _password = textoEscrito;
+          // print(_direccion);
+        } );
+      }
+
+    );
+  }//_crearEmail
+
+  Widget _ingresaPassword(){
+    return TextField(
+
+      keyboardType: TextInputType.emailAddress ,
+      //Te pone el arroba en el teclado xD
+      
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.only( bottomRight : Radius.circular(30)) ,
+        ) ,
+        counter: Text('letras ${_password.length } '),
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.security , size: 30.0 , )
+      ),
+
+      onChanged: (textoEscrito){ //recibe un valor el cual se definio en esta linea
+        setState( ()  {
+          _email = textoEscrito;
+          // print(_direccion);
+        } );
+      }
+
+    );
+  }
 
 
   Widget _revisarDatos(){
     return ListTile(
       title: Text('Revise sus datos:'),
-      subtitle: Text(' ${_direccion} '),
+      subtitle: Text('Su Direccion es: ${_direccion}\nSu email es: ${_email} '),
     );
   }
 
